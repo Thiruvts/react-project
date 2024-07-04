dc15a5ca11c6da58073d6f7dbe50e1187e19b872
Thiruvts <thiruvts@gmail.com>
2024-07-04 10:21:34 +0530

    react state hooks

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const Content = () => {
     function handlename(){
@@ -6,16 +7,29 @@ const Content = () => {
         const int = Math.floor(Math.random()*3);
         return names[int];
       }
-    const handleClick = (e) =>{
+    /*const handleClick = (e) =>{
         console.log(e)
     }
     const handleClick2 = (name) =>{
         console.log(`Thanks for the subscribe ${name}`)
+    }*/
+
+    const [count, setCount] = useState(0);
+    function incrementFunction(){
+        setCount((count) => {return count + 1});
+        setCount((count) => {return count + 1});
+        setCount((count) => {return count + 1});
     }
   return (
     <main>
-        <p onDoubleClick={()=> handleClick2('thiru')}>Lets {handlename()} Money</p>
-        <button onClick={(e) => handleClick(e)}>Subscribe</button>
+        {/* <p onDoubleClick={()=> handleClick2('thiru')}>Lets {handlename()} Money</p>
+        <button onClick={(e) => handleClick(e)}>Subscribe</button> */}
+
+        <p>Lets Earn Money</p>
+        <button>Subscribe</button>
+        <button>-</button>
+        <span>{count}</span>
+        <button onClick={incrementFunction()}>+</button>
     </main>
   ) 
 }
